12f394721f03b5e769876bcde8001941d77106c2
Daniel Alves <daniel199831.da@gmail.com>
2024-11-20 12:22:47 -0300

    meu primeiro comentario

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup> 
+import homeView from './homeView.vue';
+
+
+
+
+</script>
+
+
+<template>
+
+    <homeView />
+
+</template>
+
+
+
+
+<style>
+
+    body {
+        background-color: rgb(88, 29, 29);
+    }
+
+</style>
